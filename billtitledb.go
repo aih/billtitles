04b8b50ea4f3aa2014c72756e73aa9a7f9a09e0d
Ari Hershowitz <arihershowitz@gmail.com>
2021-08-23 16:19:55 -0700

    Remove sample db

--- billtitledb.go
+++ billtitledb.go
@@ -11,40 +11,7 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-type Product struct {
-	gorm.Model
-	Code  string
-	Price uint
-}
-
 func RunDbExample() {
-
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	// Migrate the schema
-	db.AutoMigrate(&Product{})
-
-	// Create
-	db.Create(&Product{Code: "D42", Price: 100})
-
-	// Read
-	var product Product
-	//db.First(&product, 1)                 // find product with integer primary key
-	db.First(&product, "code = ?", "D42") // find product with code D42
-
-	// Update - update product's price to 200
-	db.Model(&product).Update("Price", 200)
-	// Update - update multiple fields
-	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
-	log.Info().Msgf("product %v updated", product)
-	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
-	log.Info().Msgf("product %v updated", product)
-
-	// Delete - delete product
-	db.Delete(&product, 1)
 
 	newLogger := logger.New(
 		stdlog.New(os.Stdout, "\r\n", stdlog.LstdFlags), // io writer
@@ -139,7 +106,6 @@ func RunDbExample() {
 	// Delete - delete all items in product, bill and title tables
 	var dbill Bill
 	var dtitle Title
-	db.Delete(&product)
 	db2.Delete(&dbill)
 	db2.Delete(&dtitle)
 

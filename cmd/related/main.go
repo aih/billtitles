93d6736dd3c29945d5e4f2fb81c64193cae7a735
Ari Hershowitz <arihershowitz@gmail.com>
2021-08-29 08:35:21 -0700

    Try to avoid locking db in LoadBillsRelated

--- cmd/related/main.go
+++ cmd/related/main.go
@@ -40,7 +40,6 @@ func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	log.Debug().Msg("Log level set to Debug")
-	db := billtitles.GetRelatedDb(billtitles.BILLTITLES_DB)
-	billtitles.LoadBillsRelatedToDBFromJson(db, parentPath)
+	billtitles.LoadBillsRelatedToDBFromJson(parentPath)
 
 }
